7fe8920ab434b4eccadabff653cea8d2ee2b7cea
Austin Brummett <austin.brummett@outlook.com>
2024-11-19 16:23:11 -0600

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Frontend-Capstone/"
+  base: "/Frontend-Capstone/",
+  build: {
+    outDir: 'dist', // Ensure output directory is specified
+    rollupOptions: {
+      input: 'src/main.tsx', // Ensure correct input
+    },
+  }
 })
